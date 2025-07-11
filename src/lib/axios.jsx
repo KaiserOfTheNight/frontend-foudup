9378bc22e8322e754c0119442d0e76f804e92ac2
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 20:00:25 +0200

    monster2

--- src/lib/axios.jsx
+++ src/lib/axios.jsx
@@ -2,7 +2,4 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
   baseURL: `https://foundup.vercel.app/api/`,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  }
 });
